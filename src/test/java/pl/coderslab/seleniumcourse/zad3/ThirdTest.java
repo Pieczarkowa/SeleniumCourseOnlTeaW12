f8386132ba23f2a8a10b2dd0cc9abd21f474f32b
Pieczarkowa <paula.gotkowska@gmail.com>
2021-12-05 11:58:24 +0100

    solved zad3

---
+++ src/test/java/pl/coderslab/seleniumcourse/zad3/ThirdTest.java
@@ -0,0 +1,28 @@
+package pl.coderslab.seleniumcourse.zad3;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+import org.openqa.selenium.By;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ThirdTest {
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        driver.get("https://coderslab.pl/pl");
+        driver.navigate().back();
+        driver.get("https://mystore-testlab.coderslab.pl/index.php");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+
+    }
+
+}
