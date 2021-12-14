bb5134a56046b4022cfd3052697af097cf47f297
Pieczarkowa <paula.gotkowska@gmail.com>
2021-12-14 16:55:57 +0100

    Dzien 4 Sel.Web.Zad.1

--- src/test/java/pl/coderslab/seleniumcourse/KonfigZad1.java
+++ src/test/java/pl/coderslab/seleniumcourse/KonfigZad1.java
@@ -1,23 +1,20 @@
-package pl.coderslab.seleniumcourse.zad2;
-
+package pl.coderslab.seleniumcourse;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class
-SecondTest {
+public class KonfigZad1 {
 
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.get("https://coderslab.pl/pl");
-        driver.get("https://mystore-testlab.coderslab.pl/index.php");
-        driver.get("https://hotel-testlab.coderslab.pl/en/");
-
-
+        driver.get("https://www.google.com/");
+        WebElement searchInput = driver.findElement(By.name("q")); // nie dziala przez popup o bezpieczenstwie
+        searchInput.sendKeys("java");
+        searchInput.submit();
     }
-
 }
+
 
