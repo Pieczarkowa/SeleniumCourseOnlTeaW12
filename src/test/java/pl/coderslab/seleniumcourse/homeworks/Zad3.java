03293efa58fa98f0c13322b1a8b18b79f5d5536c
Pieczarkowa <paula.gotkowska@gmail.com>
2021-12-17 13:35:35 +0100

    Resolved task 3 homeworks

---
+++ src/test/java/pl/coderslab/seleniumcourse/homeworks/Zad3.java
@@ -0,0 +1,54 @@
+package pl.coderslab.seleniumcourse.homeworks;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+import java.time.Duration;
+
+public class Zad3 {
+
+    WebDriver driver;
+
+    @Test
+    public void submittingTheForm () {
+        driver.get("https://katalon-test.s3.amazonaws.com/demo-aut/dist/html/form.html");
+        WebElement firstNameField = driver.findElement(By.id("first-name"));
+        firstNameField.sendKeys("Karol");
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        lastName.sendKeys("Kowalski");
+        WebElement genderFieldMr = driver.findElement(By.className("radio-inline"));
+        genderFieldMr.click();
+
+        WebElement dateOfBirth = driver.findElement(By.id("dob"));
+        dateOfBirth.sendKeys("05/22/2010");
+        WebElement addressField = driver.findElement(By.id("address"));
+        addressField.sendKeys("Prosta 51");
+        WebElement emailField = driver.findElement(By.name("email"));
+        emailField.sendKeys("karol.kowalski@mailinator.com");
+        WebElement passwordField = driver.findElement(By.name("password"));
+        passwordField.sendKeys("Pass123");
+        WebElement companyField = driver.findElement(By.name("company"));
+        companyField.sendKeys("Coders Lab");
+        WebElement commentSection = driver.findElement(By.name("comment"));
+        commentSection.sendKeys("To jest mój pierwszy automat testowy");
+        WebElement submitBtn = driver.findElement(By.id("submit"));
+        submitBtn.click();
+    }
+
+
+        @BeforeEach
+                public void beforeEach() {
+            System.setProperty("webdriver.edge.driver", "src/test/resources/msedgedriver.exe");
+            this.driver = new EdgeDriver();
+            this.driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(16));
+
+        }
+
+    }
+
+
+
