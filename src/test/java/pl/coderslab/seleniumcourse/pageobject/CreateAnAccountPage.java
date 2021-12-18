0e5f8c3b73a5b944edc99995d865ea79b32a8b19
Pieczarkowa <paula.gotkowska@gmail.com>
2021-12-18 12:19:09 +0100

    Page object examples

---
+++ src/test/java/pl/coderslab/seleniumcourse/pageobject/CreateAnAccountPage.java
@@ -0,0 +1,17 @@
+package pl.coderslab.seleniumcourse.pageobject;
+
+import org.openqa.selenium.WebDriver;
+
+public class CreateAnAccountPage {
+    private WebDriver driver;
+
+    public CreateAnAccountPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void fillForm(UserData userData) {
+
+
+    }
+
+}
